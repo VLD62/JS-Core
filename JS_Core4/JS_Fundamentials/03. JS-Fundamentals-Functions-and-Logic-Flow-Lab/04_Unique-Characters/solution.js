fe9015b6198f591ef88fa06f6f32324ceaf83341
VLD62 <vld62@students.softuni.bg>
2019-01-29 23:36:16 +0200

    Adding tasks

--- JS_Core4/JS_Fundamentials/03. JS-Fundamentals-Functions-and-Logic-Flow-Lab/04_Unique-Characters/solution.js
+++ JS_Core4/JS_Fundamentials/03. JS-Fundamentals-Functions-and-Logic-Flow-Lab/04_Unique-Characters/solution.js
@@ -1,18 +1,24 @@
 function solve() {
   let string = document.getElementById("string").value;
-  let newString = string.charAt(0);
+  let newString = "";
   
   function matchCounter (char, string) {
 
     let re = new RegExp(char,"g");
     let matches = string.match(re);
-    console.log(matches);
-    return matches.length;
+    if (matches !== null) {
+      return matches.length;
+    } else {
+      return 0;
+    }
   }
   for (let i = 0; i <= string.length; i++) {
-    if (matchCounter(string.charAt(i), newString) == 1 || string[i] == " ") {
+   if ((matchCounter(string.charAt(i), string) > 0 && matchCounter(string.charAt(i), newString) == 0)
+    || string[i] == " ") {
+    
       newString += string.charAt(i);
-    } 
+    }
+    console.log(newString); 
   }
   document.getElementById('result').textContent = newString;
 }
--- JS_Core4/JS_Fundamentials/03. JS-Fundamentals-Functions-and-Logic-Flow-Lab/05_Special-Words/solution.js
+++ JS_Core4/JS_Fundamentials/03. JS-Fundamentals-Functions-and-Logic-Flow-Lab/05_Special-Words/solution.js
@@ -1,4 +1,23 @@
 function solve() {
-  // TODO
-
+  let firstNumber = +document.getElementById("firstNumber").value;
+  let secondNumber = +document.getElementById("secondNumber").value;
+  let firstString = document.getElementById("firstString").value;
+  let secondString = document.getElementById("secondString").value;
+  let thirdString = document.getElementById("thirdString").value;
+  function pAppender(text) {
+    let p = document.createElement('p');
+    p.textContent = text;
+    document.getElementById('result').appendChild(p);
+  }
+  for (let i = firstNumber; i <= secondNumber; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        pAppender(`${i} ${firstString}-${secondString}-${thirdString}`);
+      } else if (i % 3 == 0) {
+        pAppender(`${i} ${secondString}`);
+      } else if (i % 5 == 0) {
+        pAppender(`${i} ${thirdString}`);
+      } else {
+        pAppender(`${i}`);
+      }
+  }
 }
