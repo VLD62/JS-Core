57c65b528c49d4ea080fda55aeb0a501b8850e67
VLD62 <vld62@students.softuni.bg>
2019-02-05 07:28:40 +0200

    adding

--- JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Lab/02_Find-ASCII-Equivalent/solution.js
+++ JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Lab/02_Find-ASCII-Equivalent/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  // TODO
+  let str =  document.getElementById('str').value.split(" ");
+  let numArray = [];
+  let wordArray = [];
+  function pAppneder(text) {
+    let p = document.createElement('p');
+    p.textContent = text;
+    (document.getElementById('result')).appendChild(p);
+  }
+  function isNumber(str) {
+    let pattern = /^\d+$/;
+    return pattern.test(str);  
+  }
+
+  //str = str.map(ascii = a => a.charCodeAt(0));
+  let result = "";
+
+  str.forEach((element) => {
+    if(isNumber(element)) {
+      numArray.push(element);
+    } else {
+      wordArray.push(element);
+    }
+  }); 
+
+  pAppneder(String.fromCharCode(numArray.join(",")));
+  pAppneder(numArray.map(char => char.charCodeAt(0)).reduce((current, previous) => previous + current));
+
 }
