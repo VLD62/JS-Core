82bf0ee484db02b4f0ac8e8cbd3455e1ee5d48ad
Vladislav Iliev <vladislav.iliev@doitwise.com>
2019-02-06 18:53:46 +0200

    adding tasks

--- JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/01_Binary-Decoding/solution.js
+++ JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/01_Binary-Decoding/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO...
+  let sum = (document.getElementById('str').value.split("").map(x => +x).reduce((sum, current) => sum + current, 0) + "").split("").map(x => +x).reduce((sum, current) => sum + current, 0) + "";
+  while (sum.length > 1) {
+    sum = sum.split("").map(x => +x).reduce((sum, current) => sum + current, 0) + "";
+  }
+  let finalArr = [];
+  function arraySplitter(array, chunk, array2) {
+    for (let i = 0; i < array.length; i += chunk) {
+      array2.push(array.slice(i, i + chunk).join(""));
+    }
+  }
+  arraySplitter(document.getElementById('str').value.substr(+sum).slice(0, +sum * (-1)).split(""), 8, finalArr);
+  for (element of finalArr.map(x => parseInt(x, 2))) {
+    if (/\b[a-zA-Z\s]+\b|\s/.test(String.fromCharCode(element))) {
+      document.getElementById('result').textContent += String.fromCharCode(element);
+    }
+  }
 }
--- JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/02_Expedition-Problems/solution.js
+++ JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/02_Expedition-Problems/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  let indicator = document.getElementById('str').value;
+  let txt = document.getElementById('text').value;
+  let regexMessage = new RegExp("(?<=" + indicator + ")(.*?)(?=" + indicator + ")", "g");
+  let regexNorth = new RegExp(/(north)[^\d]*([0-9]{2})[^,]*,[^,]*?([0-9]{6})/, "gi");
+  let regexEast = new RegExp(/(east)[^\d]*([0-9]{2})[^,]*,[^,]*?([0-9]{6})/, "gi");
+
+  function pAppender(text) {
+    let p = document.createElement('p');
+    p.innerHTML = text;
+    document.getElementById('result').appendChild(p);
+  }
+  let north = regexNorth.exec(txt);
+  let east = regexEast.exec(txt);
+
+
+  var matches = [];
+  var match = regexNorth.exec(txt);
+  while (match != null) {
+    matches.push(match[1]);
+    match = regexNorth.exec(txt);
+  }
+  console.log(matches);
+
+  pAppender(north[2] + "." + north[3] + " " + north[1].charAt(0).toUpperCase());
+  pAppender(east[2] + "." + east[3] + " " + east[1].charAt(0).toUpperCase());
+  pAppender("Message: " + regexMessage.exec(txt)[0]);
+
 }
