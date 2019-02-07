b65293b73b3d29b013bd1cbfe3bfb62d22c640a4
Vladislav Iliev <vladislav.iliev@doitwise.com>
2019-02-07 18:03:54 +0200

    addiing

--- JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/02_Expedition-Problems/solution.js
+++ JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/02_Expedition-Problems/solution.js
@@ -2,28 +2,62 @@ function solve() {
   let indicator = document.getElementById('str').value;
   let txt = document.getElementById('text').value;
   let regexMessage = new RegExp("(?<=" + indicator + ")(.*?)(?=" + indicator + ")", "g");
-  let regexNorth = new RegExp(/(north)[^\d]*([0-9]{2})[^,]*,[^,]*?([0-9]{6})/, "gi");
-  let regexEast = new RegExp(/(east)[^\d]*([0-9]{2})[^,]*,[^,]*?([0-9]{6})/, "gi");
-
+  let regexNorthEast = new RegExp(/(north|east)[^\d]*([0-9]{2})[^,]*,[^,]*?([0-9]{6})/, "gi");
+  let norths = [];
+  let easts = [];
   function pAppender(text) {
     let p = document.createElement('p');
     p.innerHTML = text;
     document.getElementById('result').appendChild(p);
   }
-  let north = regexNorth.exec(txt);
-  let east = regexEast.exec(txt);
-
-
-  var matches = [];
-  var match = regexNorth.exec(txt);
-  while (match != null) {
-    matches.push(match[1]);
-    match = regexNorth.exec(txt);
+  let matches = txt.match(regexNorthEast);
+  for (element of matches) {
+    if (element.toUpperCase().charAt(0) == "N") {
+      norths.push(element); 
+    } else {
+      easts.push(element);
+    }
   }
-  console.log(matches);
-
-  pAppender(north[2] + "." + north[3] + " " + north[1].charAt(0).toUpperCase());
-  pAppender(east[2] + "." + east[3] + " " + east[1].charAt(0).toUpperCase());
+  let north = norths[norths.length - 1].replace(",", ".").replace(/\s*[a-zA-Z]*/g, "");
+  let east =  easts[easts.length - 1].replace(",", ".").replace(/\s*[a-zA-Z]*/g, "");
+  let northArr = north.split(".");
+  let eastArr = east.split(".");
+  northArr[0] =  northArr[0].charAt(0) + northArr[0].charAt(1);
+  eastArr[0] =  eastArr[0].charAt(0) + eastArr[0].charAt(1);
+  pAppender(northArr.join(".") + " N");
+  pAppender(eastArr.join(".") + " E");
   pAppender("Message: " + regexMessage.exec(txt)[0]);
+}
 
+function solve2() {
+
+  let inputKey = document.querySelector('#str').value;
+  let inputText = document.querySelector('#text').value;
+  let output = document.querySelector('#result');
+
+  let regExNorth = /(north|east).*?(\d{2})[^,]*?,[^,]*?(\d{6})/gi;
+  let regExMessage = new RegExp(`${inputKey}(.*?)${inputKey}`, "g");
+  let message = `Message: ${regExMessage.exec(inputText)[1]}`;
+
+  let result;
+  let north;
+  let east;
+
+  while (result = regExNorth.exec(inputText)) {
+      if (result[1].toLowerCase() === 'north') {
+          north = `${result[2]}.${result[3]} N`;
+      } else {
+          east = `${result[2]}.${result[3]} E`;
+      }
+  }
+
+  createAddAppend('p', north, output);
+  createAddAppend('p', east, output);
+  createAddAppend('p', message, output);
+
+  function createAddAppend(child, text, parent) {
+      let childElement = document.createElement(child);
+      childElement.textContent = text;
+      parent.appendChild(childElement);
+  }
 }
--- JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/04_Airport-Check/solution.js
+++ JS_Core4/JS_Fundamentials/05. JS-Fundamentals-Strings-and-RegEx-Exercise/04_Airport-Check/solution.js
@@ -1,3 +1,55 @@
 function solve() {
- //TODO...
+
+    let input = document.querySelector('#str').value;
+    let output = document.querySelector('#result');
+
+    input = input.split(', ');
+    let command = input.pop();
+    input = input.toString();
+
+    let nameValidator = /(?<=\s)([A-Z][A-Za-z]*)-([A-Z][A-Za-z]*)(?=\s)|((?<=\s)([A-Z][A-Za-z]*)-([A-Z][A-Za-z]*)\.-([A-Z][A-Za-z]*)(?=\s))/g;
+    let airportValidator = /(?<=\s)([A-Z]{3})\/([A-Z]{3})(?=\s)/g;
+    let flightValidator = /(?<=\s)([A-Z]{1,3})(\d{1,5})(?=\s)/g;
+    let companyValidator = /(?<=-\s)([A-Z][A-Za-z]*)\*([A-Z][A-Za-z]*)(?=\s)/g;
+
+    let result;
+
+    if (command === 'name') {
+
+        let passenger = validator(nameValidator);
+        output.textContent = `Mr/Ms, ${passenger}, have a nice flight!`;
+
+    } else if (command === 'flight') {
+
+        let flightNum = validator(flightValidator);
+        let airports = validator(airportValidator);
+        output.textContent = `Your flight number ${flightNum} is from ${airports[0]} to ${airports[1]}.`;
+
+    } else if (command === 'company') {
+
+        let company = validator(companyValidator);
+        output.textContent = `Have a nice flight with ${company}.`;
+
+    } else if (command === 'all') {
+
+        let passenger = validator(nameValidator);
+        let airports = validator(airportValidator);
+        let flightNum = validator(flightValidator);
+        let company = validator(companyValidator);
+        output.textContent = `Mr/Ms, ${passenger}, your flight number ${flightNum} is from ${airports[0]} to ${airports[1]}. Have a nice flight with ${company}.`;
+    }
+
+    function validator(regEx) {
+        result = regEx.exec(input);
+        if (regEx === nameValidator) {
+            while (result[0].includes('-')) {
+                result[0] = result[0].replace('-', ' ');
+            }
+        } else if (regEx === airportValidator) {
+            result[0] = result[0].split('/');
+        } else if (regEx === companyValidator) {
+            result[0] = result[0].replace('*', ' ');
+        }
+        return result[0];
+    }
 }
